75d9919d5d0832d5f07aeb44de29587402c61d87
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-05-29 11:31:31 +0200

    Add: little change searchbar

--- src/assets/components/SearchBar.jsx
+++ src/assets/components/SearchBar.jsx
@@ -1,12 +1,7 @@
-import { FaSearch } from "react-icons/fa"; // Assicurati di avere react-icons installato
-
 export default function SearchBar({ query, setQuery }) {
   return (
     <div className="container my-4">
       <div className="input-group mx-auto" style={{ maxWidth: "400px" }}>
-        <span className="input-group-text bg-warning text-white">
-          <FaSearch />
-        </span>
         <input
           type="text"
           className="form-control"
